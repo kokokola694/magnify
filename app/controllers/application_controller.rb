93fcb28f8c094c7ded8774a6f3e88f83df7059e4
Michael Ko <mk872@cornell.edu>
2018-11-13 10:51:19 -0500

    Setup using checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user
+
+  def current_user
+    User.find_by(session_token: session[:session_token])
+  end
+
+  def ensure_logged_in
+    render json: ['You must be logged in.'], status: 401
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
 end
