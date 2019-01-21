d281106dab7f2a379f51d8a31ddf721fb1c23c6c
Michael Ko <mk872@cornell.edu>
2019-01-21 17:27:42 -0500

    Fix old migration

--- db/migrate/20190111160730_create_genres.rb
+++ db/migrate/20190111160730_create_genres.rb
@@ -1,14 +1,14 @@
 class CreateGenres < ActiveRecord::Migration[5.2]
   def change
-    create_table :recents do |t|
+    create_table :recent do |t|
       t.integer :song_id, null: false
       t.integer :album_id, null: false
       t.integer :user_id, null: false
       t.timestamps
     end
-    add_index :recents, :user_id
-    add_index :recents, :song_id
-    add_index :recents, :album_id
+    add_index :recent, :user_id
+    add_index :recent, :song_id
+    add_index :recent, :album_id
     add_column :albums, :genre, :string, null: false
   end
 end
